d902c57f422f38a92b079988c197fc7f16dccadc
Sam Collier <samcollier916@gmail.com>
2024-09-14 15:56:54 +0100

    add some essential options

---
+++ plugin/options.lua
@@ -0,0 +1,27 @@
+local opt = vim.opt
+
+-- search settings
+opt.smartcase = true
+opt.ignorecase = true
+opt.hlsearch = false
+
+-- line number settings
+opt.number = true
+opt.relativenumber = true
+
+-- Don't have `o` add a comment
+opt.formatoptions:remove "o"
+
+opt.wrap = true
+opt.linebreak = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+opt.errorbells = false
+
+opt.swapfile = false
+
+opt.hidden = true
+
+opt.expandtab = false
