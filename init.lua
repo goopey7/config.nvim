1e7ce09968ad4c162f7cfb141fdb22b86058dc08
Sam Collier <samcollier916@gmail.com>
2024-09-14 15:13:55 +0100

    update comments in init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
---[[
--- Setup initial configuration,
--- 
--- Primarily just download and execute lazy.nvim
---]]
+-- setup lazy.nvim
 
+-- if we don't have lazy, download it
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
   vim.fn.system {
@@ -16,11 +13,11 @@ if not vim.uv.fs_stat(lazypath) then
   }
 end
 
--- Add lazy to the `runtimepath`, this allows us to `require` it.
+-- add lazy to the `runtimepath`, this allows us to `require` it.
 ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- Set up lazy, and load my `lua/config/plugins/` folder
+-- set up lazy, and load my `lua/config/plugins/` folder
 require("lazy").setup({ import = "config/plugins" }, {
   change_detection = {
     notify = false,
