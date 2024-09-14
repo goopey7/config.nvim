2e9d8bd664f9012b7e293d8bb2bfe7b3bbbf14d6
Sam Collier <samcollier916@gmail.com>
2024-09-14 19:54:24 +0100

    move telescope keybinds to keys

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,11 +1,12 @@
+local builtin = require "telescope.builtin"
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    local builtin = require "telescope.builtin"
-    vim.keymap.set("n", "<leader>f", builtin.find_files)
-  end,
+  keys = {
+    { "<leader>f", builtin.find_files, desc = "Find files" },
+  },
 }
