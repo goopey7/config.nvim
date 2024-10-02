e6c946868b42bed8ac5729728692d94d5f89e1b6
Sam Collier <t-samcollier@microsoft.com>
2024-10-02 17:09:14 +0100

    neovide: use source code pro nerd font

--- plugin/neovide.lua
+++ plugin/neovide.lua
@@ -3,4 +3,5 @@ if vim.g.neovide then
 	vim.g.neovide_position_animation_length = 0
 	vim.g.neovide_scroll_animation_length = 0
 	vim.g.neovide_scroll_animation_far_lines = 0
+	vim.o.guifont = "SauceCodePro Nerd Font:h18"
 end
