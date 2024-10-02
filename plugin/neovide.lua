cd2beb40b80e98f14d32b84eac636d69e255528c
Sam Collier <t-samcollier@microsoft.com>
2024-10-02 17:08:55 +0100

    neovide: disable animations

--- plugin/neovide.lua
+++ plugin/neovide.lua
@@ -1,3 +1,6 @@
 if vim.g.neovide then
 	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_position_animation_length = 0
+	vim.g.neovide_scroll_animation_length = 0
+	vim.g.neovide_scroll_animation_far_lines = 0
 end
