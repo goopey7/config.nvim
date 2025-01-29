d8d05208ad778df016f2afc63d98bb12c93c4aee
Sam Collier <samcollier916@gmail.com>
2025-01-29 21:40:24 +0000

    stylua

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,5 +3,5 @@ vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":'<,'>lua<CR>")
 
-local opt = vim.opt_local;
+local opt = vim.opt_local
 opt.shiftwidth = 2
