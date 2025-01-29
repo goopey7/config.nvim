1cd21a21000d00f405c74bc305120d4baf238afd
Sam Collier <samcollier916@gmail.com>
2025-01-29 19:22:10 +0000

    lua options

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -2,3 +2,6 @@
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>")
 vim.keymap.set("v", "<leader>x", ":'<,'>lua<CR>")
+
+local opt = vim.opt_local;
+opt.shiftwidth = 2
