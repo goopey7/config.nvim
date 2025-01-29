49986e9ddbcf07eb6f2bbf45f22c60903b010ac0
Sam Collier <samcollier916@gmail.com>
2025-01-29 19:22:06 +0000

    bindings to run lua in nvim

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+-- bindings for executing lua in neovim
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":'<,'>lua<CR>")
