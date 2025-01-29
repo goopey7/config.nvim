a832cedc3a08687e10b5c70a0fd02fad76a8c6bd
Sam Collier <samcollier916@gmail.com>
2025-01-29 21:39:19 +0000

    treesitter

---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    local languages = require("config.languages")
+    treesitter.setup({
+      ensure_installed = languages,
+      auto_install = false,
+      highlight = {
+        enable = true,
+        -- disable if loading a large file
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+    })
+  end,
+}
