61f2a3ccdbf2373f4e4c043453185bf264e30324
Sam Collier <t-samcollier@microsoft.com>
2025-02-09 01:54:45 +0000

    code completion

---
+++ lua/config/plugins/completion.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "onsails/lspkind.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      require "config.completion"
+    end,
+  },
+}
