8972fa8f5648fae99c252b969eec4135450ac9fd
Sam Collier <t-samcollier@microsoft.com>
2025-02-26 22:52:18 +0000

    new lsp keybinds + disable autoformat for now

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,14 +1,23 @@
-vim.keymap.set("n", "grn", vim.lsp.buf.rename)
-vim.keymap.set("n", "gra", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-vim.keymap.set("n", "<leader>F", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev)
 
 vim.diagnostic.config({ underline = false })
 
-local ivy = require("telescope.themes").get_ivy()
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "gd", function() builtin.lsp_definitions(ivy) end)
-vim.keymap.set("n", "grr", function() builtin.lsp_references(ivy) end)
-vim.keymap.set("n", "gt", function() builtin.lsp_workspace_symbols(ivy) end)
+vim.keymap.set("n", "gd", "<cmd>Tele lsp_definitions<CR>")
+vim.keymap.set("n", "grr", "<cmd>Tele lsp_references<CR>")
+vim.keymap.set("n", "gt", "<cmd>Tele lsp_dynamic_workspace_symbols<CR>")
 
+
+-- vim.api.nvim_create_autocmd("LspAttach", {
+-- 	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+-- 	callback = function(args)
+-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+-- 			buffer = args.buf,
+-- 			callback = function()
+-- 				vim.lsp.buf.format { async = false, id = args.data.client_id }
+-- 			end,
+-- 		})
+-- 	end
+-- })
