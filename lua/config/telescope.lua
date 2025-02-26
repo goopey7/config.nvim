dc7412bb7e7da749be6d5b50e6d38092b60f6a4a
Sam Collier <t-samcollier@microsoft.com>
2025-02-26 22:51:51 +0000

    unreal config

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,28 +1,22 @@
 local telescope = require("telescope")
 
 local config = {
-  defaults = {
-    file_ignore_patterns = {
-      "Binaries/*",
-      "Content/*",
-      "Intermediate/*",
-      "obj/*",
-      ".git/*",
-    },
-  },
-  pickers = {
-    find_files = {
-      theme = "ivy",
-    },
-    buffers = {
-      theme = "ivy",
-    },
-  },
-  extensions = {
-    ["ui-select"] = {
-      require("telescope.themes").get_ivy(),
-    },
-  },
+	defaults = {
+		path_display = { "tail" },
+	},
+	pickers = {
+		find_files = {
+			theme = "ivy",
+		},
+		buffers = {
+			theme = "ivy",
+		},
+	},
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_ivy(),
+		},
+	},
 }
 
 telescope.setup(config)
@@ -31,5 +25,5 @@ telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>fd", builtin.find_files)
 vim.keymap.set("n", "<leader>fb", builtin.buffers)
+vim.keymap.set("n", "<leader>fd", builtin.find_files)
