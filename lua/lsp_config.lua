0dd2654c54d81c4f6a7fb698a303fbe09caf095e
Sam Collier <samcollier916@gmail.com>
2025-09-10 23:28:41 +0100

    manually trigger completion

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client then
 			if client:supports_method("textDocument/completion") then
-				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
 			end
 		end
 	end
