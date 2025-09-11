1bb0e3702cc5cdd30744053431196f804fa6c913
Sam Collier <samcollier916@gmail.com>
2025-09-11 12:42:50 +0100

    toggle neovide fullscreen

--- plugin/neovide.lua
+++ plugin/neovide.lua
@@ -4,4 +4,5 @@ if vim.g.neovide then
 	vim.g.neovide_scroll_animation_length = 0
 	vim.g.neovide_scroll_animation_far_lines = 0
 	vim.o.guifont = "Source Code Pro:h16"
+	vim.keymap.set("n", "<F11>", function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
 end
