12fa3d95b5b90ec80e79724a69ce255fc2a01690
Sam Collier <samcollier916@gmail.com>
2026-05-22 18:44:11 +0100

    start over

--- plugin/neovide.lua
+++
@@ -1,8 +0,0 @@
-if vim.g.neovide then
-	vim.g.neovide_cursor_animation_length = 0
-	vim.g.neovide_position_animation_length = 0
-	vim.g.neovide_scroll_animation_length = 0
-	vim.g.neovide_scroll_animation_far_lines = 0
-	vim.o.guifont = "Source Code Pro:h16"
-	vim.keymap.set("n", "<F11>", function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
-end
