501d34394dd4a854526e3aa500a826887dbd683a
Sam Collier <samcollier916@gmail.com>
2026-05-23 01:33:44 +0100

    lua lsp

--- init.lua
+++ init.lua
@@ -1,12 +1,4 @@
 vim.o.autocomplete = true
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
-		end
-	end,
-})
 vim.opt.complete:append('o')
 vim.opt.completeopt = { 'menuone', 'noselect', 'fuzzy' }
 vim.g.mapleader = " "
@@ -27,6 +19,7 @@ vim.g.statusline = 0
 
 vim.pack.add({
 	"https://github.com/stevearc/oil.nvim",
+	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/EdenEast/nightfox.nvim",
 	"https://github.com/stevearc/quicker.nvim",
 	"https://github.com/dmtrKovalenko/fff.nvim",
@@ -37,11 +30,23 @@ vim.cmd("colorscheme carbonfox")
 require "oil_config"
 require "fff_config"
 require "terminal_config"
+require "lsp_config"
 require "quicker_config"
 require "lazygit"
 vim.keymap.set('n', '<leader>fd', function() require('fff').find_files() end, { desc = 'Find files' })
 vim.keymap.set('n', '<leader>fl', function() require('fff').live_grep() end, { desc = 'Live Grep' })
 vim.keymap.set("n", "-", ":Oil<CR>")
+vim.keymap.set("n", "<leader>b", ":ls<CR>:b ", { desc = "Switch buffer" })
+vim.keymap.set("n", "<leader>fh", ":help ", { desc = "Help tags" })
+vim.keymap.set("n", "gt", vim.lsp.buf.workspace_symbol, { desc = "Workspace symbols" })
+vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "LSP references" })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
+vim.keymap.set("n", "gf", vim.lsp.buf.format)
+vim.keymap.set("n", "gj", function() vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR }) end)
+vim.keymap.set("n", "gk", function() vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR }) end)
+vim.keymap.set("n", "gl", function() vim.diagnostic.jump({ count = 1 }) end)
+vim.keymap.set("n", "gh", function() vim.diagnostic.jump({ count = -1 }) end)
 vim.keymap.set("n", "<leader>q", function()
 	require("quicker").toggle()
 end, {
---
+++ lua/lsp_config.lua
@@ -0,0 +1,31 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
+		end
+		if client.name == "clangd" then
+			local opts = { buffer = ev.buf, silent = true }
+			vim.keymap.set("n", "gi", "<cmd>LspClangdSwitchSourceHeader<cr>", vim.tbl_extend("force", opts, { desc = "Switch Source/Header" }))
+		end
+	end,
+})
+
+local function on_jump(diagnostic)
+	if not diagnostic then return end
+	vim.schedule(function()
+		vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+	end)
+end
+vim.diagnostic.config({ underline = true, float = { source = true, border = "rounded" }, jump = { on_jump = on_jump } })
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true)
+			}
+		}
+	}
+})
+vim.lsp.enable({"lua_ls"})
