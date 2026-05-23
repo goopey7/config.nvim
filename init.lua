1efd5b8e83bc8ce65a86d86799664a800aeb0623
Sam Collier <samcollier916@gmail.com>
2026-05-23 01:01:40 +0100

    bring in my opts

--- init.lua
+++ init.lua
@@ -9,5 +9,20 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.opt.complete:append('o')
 vim.opt.completeopt = { 'menuone', 'noselect', 'fuzzy' }
+vim.g.mapleader = " "
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.swapfile = false
+vim.o.errorbells = false
+vim.o.hidden = true
+vim.o.expandtab = false
+vim.o.winborder = "rounded"
+vim.o.smartcase = true
+vim.o.ignorecase = true
+vim.o.hlsearch = false
+vim.o.autoread = true
+vim.g.statusline = 0
 
 require "terminal"
